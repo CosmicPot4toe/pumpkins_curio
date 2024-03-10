f06ed51cabc5d922c5185eb9367d7de05af56f98
CosmicPot4toe <ji.figueroa.lopez@gmail.com>
2024-03-09 22:08:53 -0300

    PAIIIINNNN
    help me

--- src/main/java/com/nmpotat/curiospump/CuriosPumpkinMod.java
+++ src/main/java/com/nmpotat/curiospump/CuriosPumpkinMod.java
@@ -1,19 +1,28 @@
 package com.nmpotat.curiospump;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraftforge.api.distmarker.Dist;
+import com.nmpotat.curiospump.integration.CurioPumpkin;
+import net.minecraft.core.Direction;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import top.theillusivec4.curios.api.CuriosCapability;
+import top.theillusivec4.curios.api.type.capability.ICurio;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(CPumpConst.MOD_ID)
@@ -22,45 +31,30 @@ public class CuriosPumpkinMod
     public CuriosPumpkinMod() {
         CPumpCommon.init();
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        eventBus.addListener(this::setup); // Register the commonSetup method for modloading
-
-				// CPumpConst.BLOCKS.register(modEventBus); //register deferred registers on the event bus so things actually happen
-				// CPumpConst.ITEMS.register(modEventBus);
-				// CPumpConst.CREATIVE_MODE_TABS.register(modEventBus);
-
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
-
-        // Register the item to a creative tab
-        // modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        eventBus.addListener(this::setup);
     }
     private void setup(final FMLCommonSetupEvent event) {
         // Some common setup code
-        CPumpConst.LOG.info("HELLO FROM COMMON SETUP");
-
-        if (Config.logDirtBlock)
-            CPumpConst.LOG.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.CARVED_PUMPKIN));
-
-        CPumpConst.LOG.info(Config.magicNumberIntroduction + Config.magicNumber);
-
-        Config.items.forEach((item) -> CPumpConst.LOG.info("ITEM >> {}", item.toString()));
+        MinecraftForge.EVENT_BUS.addGenericListener(ItemStack.class,this::attachCapabilities);
+    }
+    private void attachCapabilities(final AttachCapabilitiesEvent<ItemStack> evt){
+        ItemStack stack = evt.getObject();
+        if (CPumpCommon.IS_PUMPKIN.test(stack)){
+            final LazyOptional<ICurio> pumpkinCurio = LazyOptional.of(()-> new CurioPumpkin(stack));
+            evt.addCapability(CuriosCapability.ID_ITEM, new ICapabilityProvider() {
+                @NotNull
+                @Override
+                public <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap,
+                                                         @Nullable Direction side) {
+                    return CuriosCapability.ITEM.orEmpty(cap,pumpkinCurio);
+                }
+            });
+            evt.addListener(pumpkinCurio::invalidate);
+        }
     }
 
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    /*@SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-    }*/
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-    /*@Mod.EventBusSubscriber(modid = CPumpConst.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents
-    {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-        }
-    }*/
+    @SubscribeEvent
+    public void gatherData(GatherDataEvent evt){
+        
+    }
 }
--- src/main/java/com/nmpotat/curiospump/integration/CurioPumpkin.java
+++ src/main/java/com/nmpotat/curiospump/integration/CurioPumpkin.java
@@ -1,7 +1,6 @@
 package com.nmpotat.curiospump.integration;
 
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.monster.EnderMan;
 import net.minecraft.world.item.ItemStack;
 import org.checkerframework.checker.nullness.qual.NonNull;
@@ -11,9 +10,7 @@ import top.theillusivec4.curios.api.type.capability.ICurio;
 import java.util.UUID;
 
 public class CurioPumpkin implements ICurio {
-	public static final AttributeModifier PUMPKIN_CURIO_MODIFIER =
-			new AttributeModifier(UUID.fromString("2a38e1a7-9ed4-4241-ab88-734b850666c2"),
-					"Carved Pumpkin Curio Modifier", 1.0D, AttributeModifier.Operation.ADDITION);
+
 	private final ItemStack stack;
 	public CurioPumpkin(ItemStack stack){
 		this.stack=stack;
